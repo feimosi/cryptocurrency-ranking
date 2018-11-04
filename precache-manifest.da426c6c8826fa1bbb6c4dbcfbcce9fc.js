c8f71291818818bfd4ff36fee7835c2e85814f2a
Marek Grzybek <feimosi@onet.pl>
2018-11-04 14:17:23 +0100

    Updates

---
+++ precache-manifest.da426c6c8826fa1bbb6c4dbcfbcce9fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "09ad22619bd538b887d4",
+    "url": "/cryptocurrency-ranking/static/css/main.747615bc.chunk.css"
+  },
+  {
+    "revision": "09ad22619bd538b887d4",
+    "url": "/cryptocurrency-ranking/static/js/main.09ad2261.chunk.js"
+  },
+  {
+    "revision": "a76b7312a9d11ac8352d",
+    "url": "/cryptocurrency-ranking/static/css/homePage.54e1f578.chunk.css"
+  },
+  {
+    "revision": "a76b7312a9d11ac8352d",
+    "url": "/cryptocurrency-ranking/static/js/homePage.a76b7312.chunk.js"
+  },
+  {
+    "revision": "08998b07d83325a3f852",
+    "url": "/cryptocurrency-ranking/static/css/2.a3c5684a.chunk.css"
+  },
+  {
+    "revision": "08998b07d83325a3f852",
+    "url": "/cryptocurrency-ranking/static/js/2.08998b07.chunk.js"
+  },
+  {
+    "revision": "5ecf14954e499995f61b",
+    "url": "/cryptocurrency-ranking/static/js/3.5ecf1495.chunk.js"
+  },
+  {
+    "revision": "aae01be01d3c689f48f0",
+    "url": "/cryptocurrency-ranking/static/js/runtime~main.aae01be0.js"
+  },
+  {
+    "revision": "4d411ca619dcac24fdc8dee6df35d042",
+    "url": "/cryptocurrency-ranking/index.html"
+  }
+];
